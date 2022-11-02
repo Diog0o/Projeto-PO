1b21329676a8136dff1ea24c728f495814a109a0
Diog0o <75538603+Diog0o@users.noreply.github.com>
2022-11-02 18:06:02 +0000

    Delete prr-core directory

--- prr-core/src/prr/exceptions/MissingFileAssociationException.java
+++
@@ -1,8 +0,0 @@
-package prr.exceptions;
-
-/** Thrown when an application is not associated with a file. */
-public class MissingFileAssociationException extends Exception {
-
-	private static final long serialVersionUID = 202208091753L;
-
-}
